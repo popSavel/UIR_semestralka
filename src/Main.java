decfbc47c58cd4f9513d2f7dae6b5eea7b3eae46
popSavit <dav.savel@gmail.com>
2022-05-03 18:39:07 +0200

    nacteni

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,35 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
+
     public static void main(String[] args) {
-        System.out.println("UIR");
+        String [] lines = readFile(args[0]);
+
+    }
+
+    private  static String[] readFile(String fileName) {
+        Path path  = Path.of(fileName);
+        ArrayList <String> lines = new ArrayList<String>();
+        try {
+            Scanner sc = new Scanner(path);
+            while(sc.hasNextLine()){
+                lines.add(sc.nextLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Error loading input file, program ended manually");
+            System.exit(0);
+        }
+        String [] result = new String[lines.size()];
+        for(int i = 0; i < result.length; i++){
+            result[i] = lines.get(i);
+        }
+
+        return result;
     }
 
 }
